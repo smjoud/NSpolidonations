52599a91212c0b87217c93351b0ee573f68601d6
seanjoudry <sean.joudry@nspccaucus.ca>
2023-02-27 09:56:09 -0400

    Updated entire service

--- script.js
+++ script.js
@@ -13,25 +13,25 @@ fetch('data.csv')
   // Get the form element
   const form = document.querySelector('form');
 
-  // Add an event listener to the input field in the form
-  const input = form.querySelector('input');
-  input.addEventListener('keyup', () => {
-    // Get the search term and split it into words
-    const searchTerm = input.value.toLowerCase();
-    const words = searchTerm.split(' ');
+            // Add an event listener to the input field in the form
+            const input = form.querySelector('input');
+            input.addEventListener('keyup', () => {
+              // Get the search term and split it into words
+              const searchTerm = input.value.toLowerCase();
+              const words = searchTerm.split(' ');
 
-    // Search the first and second column for the search terms
-    let results;
-    if (words.length === 2) {
-      results = lines.filter(line =>
-        line.split(',')[0].toLowerCase().startsWith(words[0]) &&
-        line.split(',')[1].toLowerCase().startsWith(words[1])
-      );
-    } else {
-      results = lines.filter(line =>
-        line.split(',')[0].toLowerCase().startsWith(words[0])
-      );
-    }
+              // Search the first and second column for the search terms
+              let results;
+              if (words.length === 2) {
+                results = lines.filter(line =>
+                  line.split(',')[0].toLowerCase().startsWith(words[0]) &&
+                  line.split(',')[1].toLowerCase().startsWith(words[1])
+                );
+              } else {
+                results = lines.filter(line =>
+                  line.split(',')[0].toLowerCase().startsWith(words[0])
+                );
+              }
 
     // Get the column names from the first line
     const columnNames = lines[0].split(',');
@@ -44,72 +44,149 @@ fetch('data.csv')
       // Show the table if the search input is not empty
       document.querySelector('table').style.display = 'inline-table';
 
-      // Add the column names to the table
-      const columnNamesRow = document.querySelector('#column-names');
-      columnNamesRow.innerHTML = '';
-      columnNames.forEach(name => {
-        const th = document.createElement('th');
-        th.innerText = name;
-        columnNamesRow.appendChild(th);
-      });
+// Add the column names to the table
+const columnNamesRow = document.querySelector('#column-names');
+columnNamesRow.innerHTML = '';
+columnNames.slice(0, 5).concat(columnNames.slice(6)).forEach(name => {
+  const th = document.createElement('th');
+  th.innerText = name;
+  columnNamesRow.appendChild(th);
+
+    // Add a click event listener to the header cell
+    th.addEventListener('click', () => {
+      sortTable(index);
+    });
+
+    
+});
+
+// Get all the table headers
+const tableHeaders = document.querySelectorAll("th");
+
+// Add click event listener to each header
+tableHeaders.forEach((header) => {
+  header.addEventListener("click", () => {
+    const table = header.closest("table");
+    const tbody = table.querySelector("tbody");
+    const headerIndex = [...header.parentNode.children].indexOf(header);
+    const rows = Array.from(tbody.querySelectorAll("tr"));
+    const sortDirection = header.dataset.sortDirection === "asc" ? "desc" : "asc";
+
+    // Sort the rows based on the clicked header
+    const sortedRows = rows.sort((a, b) => {
+      const aValue = a.querySelector(`td:nth-child(${headerIndex + 1})`).textContent.trim();
+      const bValue = b.querySelector(`td:nth-child(${headerIndex + 1})`).textContent.trim();
+      if (aValue < bValue) return sortDirection === "asc" ? -1 : 1;
+      if (aValue > bValue) return sortDirection === "asc" ? 1 : -1;
+      return 0;
+    });
+
+    // Update the table with the sorted rows
+    tbody.innerHTML = "";
+    sortedRows.forEach((row) => tbody.appendChild(row));
+    header.dataset.sortDirection = sortDirection;
+  });
+});
 
 
 
-      // Display the results in a table
-      const resultsTable = document.querySelector('#results');
-      resultsTable.innerHTML = '';
-      results.forEach(result => {
-        const tr = document.createElement('tr');
-        
-        // Color the row blue if the fifth column contains "PC"
-        if (result.split(',')[4] === "PC") {
-          tr.style.backgroundColor = "#639ee0";
-        }
 
-        if (result.split(',')[4] === "NDP") {
-          tr.style.backgroundColor = "orange";
-        }
 
-        if (result.split(',')[4] === "Liberal") {
-          tr.style.backgroundColor = "red";
-        }
-        
-        result.split(',').forEach(column => {
-          const td = document.createElement('td');
-          td.innerText = column;
-          tr.appendChild(td);
-        });
-        resultsTable.appendChild(tr);
+// Display the results in a table
+const resultsTable = document.querySelector('#results');
+resultsTable.innerHTML = '';
+results.forEach(result => {
+  const tr = document.createElement('tr');
 
-        
-      });
+  // Color the row blue if the fourth column contains "PC"
+  if (result.split(',')[4] === "PC") {
+    tr.style.backgroundColor = "#639ee0";
+  }
+
+    // Color the row red if the third column contains "Liberal"
+    if (result.split(',')[4].includes("Liberal")) {
+      tr.style.backgroundColor = "red";
+    }
+
+    if (result.split(',')[4].includes("NDP")) {
+      tr.style.backgroundColor = "orange";
+    }
+
+    if (result.split(',')[4].includes("Green")) {
+      tr.style.backgroundColor = "green";
+    }
+  
+    if (result.split(',')[4].includes("Atlantica")) {
+      tr.style.backgroundColor = "purple";
+    }
+  
+  
+  // Iterate over each column, skipping the 5th column but including the 6th column
+  const columns = result.split(',');
+  for (let i = 0; i < columns.length; i++) {
+    if (i !== 5) {
+      const td = document.createElement('td');
+      td.innerText = columns[i];
+      tr.appendChild(td);
+    }
+  }
+
+  resultsTable.appendChild(tr);
+});
+
 
       
       
     }
 // Define a function to calculate and display the summary information
 const updateSummary = () => {
-  // Get the number of donations from the number of rows in the results table
-  const numberOfDonations = document.querySelectorAll('#results tr').length; // subtract 1 to exclude the header row
+  const numberOfDonations = document.querySelectorAll('#results tr').length;
 
-  const totalAmount = Array.from(document.querySelectorAll('#results td:nth-child(4)')) // get the values in the fourth column
-  .map(td => parseInt(td.innerText))
-  .filter(amount => !isNaN(amount)) // filter out NaN values
-  .reduce((acc, amount) => acc + amount, 0);
+  const pcTotal = Array.from(document.querySelectorAll('#results tr td:nth-child(5)'))
+    .filter(td => td.innerText === 'PC')
+    .map(td => parseInt(td.previousElementSibling.innerText))
+    .reduce((total, amount) => total + amount, 0)
+    .toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
+  const ndpTotal = Array.from(document.querySelectorAll('#results tr td:nth-child(5)'))
+    .filter(td => td.innerText === 'NDP')
+    .map(td => parseInt(td.previousElementSibling.innerText))
+    .reduce((total, amount) => total + amount, 0)
+    .toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
+  const liberalTotal = Array.from(document.querySelectorAll('#results tr td:nth-child(5)'))
+    .filter(td => td.innerText === 'Liberal')
+    .map(td => parseInt(td.previousElementSibling.innerText))
+    .reduce((total, amount) => total + amount, 0)
+    .toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
-  // Display the number of donations and the total amount in the summary box
-  const numberOfDonationsElement = document.querySelector('#number-of-donations span');
-  numberOfDonationsElement.innerText = numberOfDonations;
+  document.querySelector('#number-of-donations').innerText = numberOfDonations;
+  document.querySelector('#pc-total').innerText = pcTotal;
+  document.querySelector('#ndp-total').innerText = ndpTotal;
+  document.querySelector('#liberal-total').innerText = liberalTotal;
 
-  const totalAmountElement = document.querySelector('#total-amount span');
-  totalAmountElement.innerText = totalAmount;
-
-  // Show the summary box
   const summaryBox = document.querySelector('#summary-box');
   summaryBox.style.display = 'block';
 };
+
+// Define a function to sort the table by a given column
+const sortTable = (table, column, asc = true) => {
+  // Code to sort the table...
+};
+
+// Add event listeners to the table headers to sort the table when clicked
+const tableHeaders = document.querySelectorAll('#results th');
+tableHeaders.forEach(header => {
+  header.addEventListener('click', () => {
+    const column = header.dataset.column;
+    const asc = header.dataset.asc === 'true';
+
+    sortTable(resultsTable, column, asc);
+
+    header.dataset.asc = (!asc).toString();
+  });
+});
+
 
 
 // Call the updateSummary function after the results table has been updated with the search results
@@ -119,7 +196,13 @@ input.addEventListener('keyup', () => {
   // Call the updateSummary function after the results table has been updated only if there are search results
   if (results.length > 0) {
     updateSummary();
+
+    
   }
+
+
+  //I think this goes here but I'm not sure
+  
 });
 
   });
@@ -128,6 +211,8 @@ input.addEventListener('keyup', () => {
 
   
 });
+
+
 
 
 
